818e64390e7793df8e60e18c6ac42a0071dae42d
jasminc815 <jasmincarmona815@gmail.com>
2026-09-04 08:00:15 +0000

    step 3 complete

--- w4/app.js
+++ w4/app.js
@@ -1,10 +1,11 @@
 import * as orderHandler from "./order-handler.js";
+import * as priceCalculator from './price-calculator.js';
+
 
 
 const orderForm = document.getElementById('order-form');
 const orderSummary = document.getElementById('order-summary');
-
-
+const orders = []
 
 const handleOrderSubmit = function (event) {
     
@@ -12,8 +13,16 @@ const handleOrderSubmit = function (event) {
 
 
         const orderData = orderHandler.getOrderInputs();
+        const calculatedPrice = priceCalculator.calculateTotal(orderData)
+        const newOrder = {
+            ...orderData,
+            ...calculatedPrice,
+            timestamp: new Date().toISOString()
+        }
 
-
+        orders.push(newOrder)
+       console.log("Order History:", orders);
+       
   
     let summaryText = `Ordered ${orderData.qty} ${orderData.size} T-Shirt${orderData.qty > 1 ? 's' : ''}`;
 
